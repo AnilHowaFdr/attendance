0a345edc96ea7a321374fa489542ee06f6ca32a5
Heron Mia <howa.anil@gmail.com>
2025-05-22 21:19:43 +0600

    firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD9Pi90GQM37o6TX-VMcyskhVYrYEerMbU",
+  authDomain: "attendance-6e9d0.firebaseapp.com",
+  databaseURL: "https://attendance-6e9d0-default-rtdb.firebaseio.com",
+  projectId: "attendance-6e9d0",
+  storageBucket: "attendance-6e9d0.firebasestorage.app",
+  messagingSenderId: "943058430257",
+  appId: "1:943058430257:web:df4f55715b7204c1032810",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
