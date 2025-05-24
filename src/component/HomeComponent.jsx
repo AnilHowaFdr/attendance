dad80cbe880d158cf944c9bf7fd1dc13728a56ca
Heron Mia <howa.anil@gmail.com>
2025-05-24 22:11:11 +0600

    delete

--- src/component/HomeComponent.jsx
+++ src/component/HomeComponent.jsx
@@ -5,24 +5,26 @@ const HomeComponent = () => {
   const db = getDatabase();
   const [name, setName] = useState("");
   const [studentList, setStudentList] = useState([]);
+  const [realtime, setRealtime] = useState(false);
   const handleSubmit = () => {
-    set(
-      push(ref(db, "user/"), {
-        name: name,
-      })
-    );
-    setName("");
+    set(push(ref(db, "user/")), {
+      name: name,
+    }).then(() => {
+      setName("");
+      setRealtime(!realtime);
+    });
   };
 
   useEffect(() => {
     let arr = [];
     onValue(ref(db, "user/"), (snapshot) => {
       snapshot.forEach((item) => {
-        arr.push(item.val());
+        arr.push({ ...item.val(), id: item.key });
       });
       setStudentList(arr);
     });
-  }, []);
+  }, [realtime]);
+  console.log(studentList);
   return (
     <div className="flex flex-col items-center justify-center h-[800px]">
       <h1 className="font-bold text-2xl pb-10 ">Student Attendance</h1>
@@ -39,7 +41,15 @@ const HomeComponent = () => {
         >
           Submit
         </button>
-        <ul className="pt-14">
+        <div className="flex flex-col gap-2">
+          {studentList.map((item) => (
+            <div className="flex items-center gap-3" key={item.id}>
+              <div className="w-[180px]">{item.name}</div>
+              <button className="p-2 bg-red-500 rounded-md">Delete</button>
+            </div>
+          ))}
+        </div>
+        {/* <ul className="pt-14">
           <li>
             <input type="text" className="border rounded-md p-2" />
             <button className="p-2 bg-blue-600 rounded-md ml-3 font-semibold text-white">
@@ -49,7 +59,7 @@ const HomeComponent = () => {
               Absent
             </button>
           </li>
-        </ul>
+        </ul> */}
       </div>
     </div>
   );
