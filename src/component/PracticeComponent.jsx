3d738e035eaaeb7f60e8b654170a50543a8024a0
Heron Mia <howa.anil@gmail.com>
2025-05-25 23:51:16 +0600

    practice

--- src/component/PracticeComponent.jsx
+++ src/component/PracticeComponent.jsx
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
-import { getDatabase, push, ref, set } from "firebase/database";
+import React, { useEffect, useState } from "react";
+import {
+  getDatabase,
+  push,
+  ref,
+  set,
+  onValue,
+  remove,
+} from "firebase/database";
 
 const PracticeComponent = () => {
   const db = getDatabase();
   const [name, setName] = useState("");
+  const [student, setStudent] = useState([]);
   const handleSubmit = () => {
     console.log(name);
     set(push(ref(db, "names/")), {
@@ -12,6 +20,23 @@ const PracticeComponent = () => {
       setName("");
     });
   };
+
+  const handleDelete = (id) => {
+    console.log(id);
+    remove(ref(db, "names/" + id)).then(() => {});
+  };
+
+  useEffect(() => {
+    let arr = [];
+    onValue(ref(db, "names/"), (snapshot) => {
+      snapshot.forEach((item) => {
+        console.log(item.val());
+        arr.push({ ...item.val(), id: item.key });
+      });
+      setStudent(arr);
+    });
+  }, []);
+  // console.log(student);
 
   return (
     <div className="flex items-center text-lg capitalize justify-center h-[800px]">
@@ -30,11 +55,18 @@ const PracticeComponent = () => {
             submit
           </button>
         </div>
-        <div className="flex items-center gap-2">
-          <p className="w-[200px]">name</p>
-          <button className="ml-2 p-2 rounded-md bg-red-500 text-white">
-            Delete
-          </button>
+        <div className="flex flex-col items-center gap-2 mt-5">
+          {student.map((item) => (
+            <div key={item.id} className="flex items-center justify-between">
+              <p className="w-[200px]">{item.name}</p>
+              <button
+                onClick={() => handleDelete(item.id)}
+                className="cursor-pointer ml-2 p-2 rounded-md bg-red-500 text-white"
+              >
+                Delete
+              </button>
+            </div>
+          ))}
         </div>
       </div>
     </div>
